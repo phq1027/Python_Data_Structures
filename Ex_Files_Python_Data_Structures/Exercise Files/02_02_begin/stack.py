070127faa56f4163def5aad15f2cba56e6eedba4
hanqin Peng <hanqinp@gmail.com>
2025-08-19 17:40:40 +0800

    adding stack.py

--- Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/stack.py
+++ Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/stack.py
@@ -6,4 +6,38 @@ Robin Andrews - https://compucademy.net/
 
 
 class Stack:
-    pass
+    def __init__(self):
+        self.stack = []
+
+    def pop(self):
+        return self.stack.pop()
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def is_empty(self):
+        return not self.stack
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def __str__(self):
+        return str(self.stack)
+
+
+if __name__ == "__main__":
+    s = Stack()
+    print(s)
+    print(s.is_empty())
+    s.push(3)
+    print(s)
+    s.push(7)
+    s.push(5)
+    print(s)
+    print(s.pop())
+    print(s)
+    print(s.peek())
+    print(s.size())
